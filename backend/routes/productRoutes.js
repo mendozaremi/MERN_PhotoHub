d1dafb10abe74caef16ab2b1f84b56f5157900e6
mendozaremi <mendozaremi@gmail.com>
2021-10-09 11:47:58 -0400

    routes complete backend done

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAllProducts, getProductById} = require('../controller/productControllers')
+
+// GET ALL PRODUCTS FROM DB
+// GET /api/products
+// access Public
+router.get('/', getAllProducts)
+
+// GET A PRODUCT BY ID FROM DB
+// GET /api/products/:id
+// access Public
+router.get('/:id', getProductById)
+
+module.exports = router;
