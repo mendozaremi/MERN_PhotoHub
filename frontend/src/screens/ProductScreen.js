d00136ac78c6f38dcc73cb06722327b047b83602
mendozaremi <mendozaremi@gmail.com>
2021-10-12 20:18:19 -0400

    product screen styling complete

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,39 @@ import './ProductScreen.css'
 const ProductScreen = () => {
     return (
         <div className="productscreen">
-            productScreen
+            <div className="productscreen__left">
+              <div className="left__image">
+                <img src="https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="product name" />
+              </div>
+
+              <div className="left__info">
+                <p className="left__name">Product 1</p>
+                <p>Price $499.99</p>
+                <p>Description: lorem ipsum</p>
+              </div>
+            </div>
+            <div className="productscreen__right">
+              <div className="right__info">
+                <p>
+                  Price: <span>$499.99</span>
+                </p>
+                <p>
+                  Status: <span>In Stock</span>
+                </p>
+                <p>
+                  Qty
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </p>
+                <p>
+                  <button type="button">Add To Cart</button>
+                </p>
+              </div>
+            </div>
         </div>
     )
 }
