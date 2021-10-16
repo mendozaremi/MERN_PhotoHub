d67d6537cb35edc48137dd7b04358bd98216e178
mendozaremi <mendozaremi@gmail.com>
2021-10-16 12:14:56 -0400

    predeploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
+const path = require('path')
 
 connectDB();
 
@@ -13,8 +14,20 @@ app.get("/", (req, res) => {
   res.json({ message: "API running..." });
 });
 
+// Serve static assests if in production
+if(process.env.NODE_ENV === 'prduction') {
+  // Set static folder
+  app.use(express.static('frontend/build'))
+
+  app.use.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build, index.html'))
+  });
+}
+
 app.use("/api/products", productRoutes)
 
+// Port
 const PORT = process.env.PORT || 5000;
  
+// Listen
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
