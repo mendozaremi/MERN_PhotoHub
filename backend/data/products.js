112f665b96c40d8acf15bc7021a7a49737a72183
mendozaremi <mendozaremi@gmail.com>
2021-10-17 18:47:17 -0400

    fixing typo

--- backend/data/products.js
+++ backend/data/products.js
@@ -15,7 +15,7 @@ const products = [
     description:
       "Yankee fan always loses by 1",
     price: 666,
-    countInStock: 1,
+    countInStock: 2,
   },
   {
     name: "el herbio nikolai jorge",
